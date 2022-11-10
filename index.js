4a9c4f0dc560f839bd61a1cda81c61c17b43b832
hachinrayhan <rayhan.hachin@gmail.com>
2022-11-10 08:52:18 +0600

    contents api created

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.j7l4khx.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        const contentsCollection = client.db('hrwindow').collection('contents');
+
+        app.get('/contents', async (req, res) => {
+            const query = {};
+            const cursor = contentsCollection.find(query);
+            const contents = await cursor.limit(0).toArray();
+            res.send(contents);
+        })
+    } finally {
+
+    }
+}
+run().catch(err => console.log(err));
 
 
 app.get('/', (req, res) => {
